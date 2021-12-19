2561d64c42f99d19e8367f0ec8784f0a544311f3
Leonardo Bertele Tosin <leob.tosin@hotmail.com>
2021-12-19 19:25:19 -0300

    GET - list all users endpoint

--- src/server/routes.ts
+++ src/server/routes.ts
@@ -8,6 +8,7 @@ routes.get('/', authenticateUser, (req, res) => {
     res.send('Made with 💙 by TheLe0');
 });
 
+routes.get('/user', authenticateUser, UserController.listAll);
 routes.post('/user', authenticateUser, UserController.create);
 routes.put('/user', authenticateUser, UserController.update);
 routes.post('/user/login', UserController.login);
