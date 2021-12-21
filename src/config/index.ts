f9ec44a6eff970c5c5911e69158ca62d836590b8
Leonardo Bertele Tosin <leob.tosin@hotmail.com>
2021-12-21 20:06:36 -0300

    Create an initial super user

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,3 +2,4 @@ export { serverConfig as ServerVars } from './server';
 export { encryptConfig as EncryptVars } from './encrypt';
 export { authConfig as AuthVars } from './auth';
 export { databaseConfig as DatabaseVars } from './database';
+export { superUserConfig as SuperUserVars } from './superUser';
